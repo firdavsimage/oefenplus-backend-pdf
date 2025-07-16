de830e0314ee385c5f1cf32ecb51beb1b042ebee
Firdavs Omonov <113253299+firdavsimage@users.noreply.github.com>
2025-07-16 13:32:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,36 +26,42 @@ def allowed_file(filename):
 def convert_file():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
+    
+    files = request.files.getlist('file')
+    if not files or files[0].filename == '':
         return jsonify({'error': 'No selected file'}), 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(input_path)
 
-        ext = filename.rsplit('.', 1)[1].lower()
-        output_filename = filename.rsplit('.', 1)[0] + '.pdf'
-        output_path = os.path.join(app.config['OUTPUT_FOLDER'], output_filename)
+    ext = files[0].filename.rsplit('.', 1)[1].lower()
+    output_filename = 'converted.pdf'
+    output_path = os.path.join(app.config['OUTPUT_FOLDER'], output_filename)
 
-        try:
-            if ext in ['jpg', 'jpeg', 'png']:
-                convert_image_to_pdf(input_path, output_path)
-            elif ext == 'docx':
-                convert_docx_to_pdf(input_path, output_path)
-            elif ext == 'pptx':
-                convert_pptx_to_pdf(input_path, output_path)
-            elif ext == 'xlsx':
-                convert_excel_to_pdf(input_path, output_path)
+    try:
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(input_path)
             else:
-                return jsonify({'error': 'Unsupported file format'}), 400
+                return jsonify({'error': 'Invalid file'}), 400
 
-            return send_file(output_path, as_attachment=True)
-        finally:
-            shutil.rmtree(UPLOAD_FOLDER)
-            shutil.rmtree(OUTPUT_FOLDER)
-            os.makedirs(UPLOAD_FOLDER)
-            os.makedirs(OUTPUT_FOLDER)
+        if ext in ['jpg', 'jpeg', 'png']:
+            convert_image_to_pdf(app.config['UPLOAD_FOLDER'], output_path)
+        elif ext == 'docx':
+            convert_docx_to_pdf(input_path, output_path)
+        elif ext == 'pptx':
+            convert_pptx_to_pdf(input_path, output_path)
+        elif ext == 'xlsx':
+            convert_excel_to_pdf(input_path, output_path)
+        else:
+            return jsonify({'error': 'Unsupported file format'}), 400
+
+        return send_file(output_path, as_attachment=True)
+    finally:
+        shutil.rmtree(UPLOAD_FOLDER)
+        shutil.rmtree(OUTPUT_FOLDER)
+        os.makedirs(UPLOAD_FOLDER)
+        os.makedirs(OUTPUT_FOLDER)
+
     else:
         return jsonify({'error': 'Invalid file'}), 400
 
